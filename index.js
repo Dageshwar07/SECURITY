438a12ba4a87bbe170cdd3cca9476d31ec907b6e
Dageshwar Das <109011384+Dageshwar07@users.noreply.github.com>
2024-10-30 10:56:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 
-const secretKey = 'dageshwar';
+const secretKey = 'dageshwarDas';
 
 app.post('/login', (req, res) => {
   const user = { id: 1, username: 'Dageshwar Das' }; 
